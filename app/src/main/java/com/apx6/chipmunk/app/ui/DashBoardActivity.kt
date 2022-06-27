5f8ea771cce90c009d2b49eb59bba82584a11e72
hnjeong <hnjeong@mailplug.co.kr>
2022-06-27 13:43:33 +0900

    [+] Add Kakao Login SDK

--- app/src/main/java/com/apx6/chipmunk/app/ui/DashBoardActivity.kt
+++ app/src/main/java/com/apx6/chipmunk/app/ui/DashBoardActivity.kt
@@ -8,7 +8,11 @@ import com.apx6.chipmunk.R
 import com.apx6.chipmunk.app.ui.base.BaseActivity
 import com.apx6.chipmunk.databinding.ActivityCoreBinding
 import com.google.android.material.snackbar.Snackbar
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -30,6 +34,37 @@ class DashBoardActivity : BaseActivity<ActivityCoreBinding>() {
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+
+        binding.btnKakaoLogin.setOnClickListener {
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                    if (error != null) {
+//                        Log.e(TAG, "카카오톡으로 로그인 실패", error)
+
+                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+
+                        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                    } else if (token != null) {
+//                        Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
+        }
+    }
+
+    val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (error != null) {
+//            Log.e(TAG, "카카오계정으로 로그인 실패", error)
+        } else if (token != null) {
+//            Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
         }
     }
 
