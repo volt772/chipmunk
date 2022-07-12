5d0c6781d0aaf8f010bfd11c4d52d0570e0b8918
hnjeong <hnjeong@mailplug.co.kr>
2022-07-12 14:59:50 +0900

    [*] With Delay

--- app/src/main/java/com/apx6/chipmunk/app/ui/SplashActivity.kt
+++ app/src/main/java/com/apx6/chipmunk/app/ui/SplashActivity.kt
@@ -9,6 +9,8 @@ import com.apx6.chipmunk.app.ext.statusBar
 import com.apx6.chipmunk.app.ui.base.BaseActivity
 import com.apx6.chipmunk.databinding.ActivitySplashBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
 
 
@@ -31,6 +33,7 @@ class SplashActivity : BaseActivity<SplashViewModel, ActivitySplashBinding>() {
         lifecycleScope.launchWhenStarted {
             viewModel.user.collect { user ->
                 user?.let {
+                    delay(1000)
                     moveToDashBoard()
                 } ?: run {
                     moveToLogin()
